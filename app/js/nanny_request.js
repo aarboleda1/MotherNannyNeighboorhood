111da625d5fe51ae88e19dffcdee950034351ee7
Anton <anton.arboleda2gmail.com>
2016-10-23 10:38:41 -0700

    added mapping feature

--- app/js/nanny_request.js
+++ app/js/nanny_request.js
@@ -1,7 +1,32 @@
 $( document ).ready(function() {
  
- 	 $( function() {
-    	$( ".datepicker" ).datepicker();
-  	} );
  
+
+
+    $(function() {
+	    $("#map").googleMap();
+	    
+	    // Marker 1
+	    $("#map").addMarker({
+	    	 coords: [37.787601, -122.396643]
+	    });
+	    
+	    // Marker 2
+	    $("#map").addMarker({
+	    	 coords: [37.783697, -122.408966]
+	    });
+		
+	    // Marker 3
+	    $("#map").addMarker({
+	        coords: [37.772930, -122.437515]
+	    });
+  })
+ 	
+ 	$( function() {
+    	$( "#datepicker" ).datepicker({
+      		changeMonth: true,
+      		changeYear: true
+    	});
+  	});
+
 });
